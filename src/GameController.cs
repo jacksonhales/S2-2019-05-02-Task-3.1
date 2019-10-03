232c60aaa018545c9598ea829fd2dff33edc6dae
Alex <alexamorri@gmail.com>
2019-10-03 11:48:58 +1000

    Updated to EasyAI equivilent

--- src/GameController.cs
+++ src/GameController.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualBasic;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-//using System.Data;
+using System.Data;
 using System.Diagnostics;
 using SwinGameSDK;
 
@@ -76,6 +76,9 @@ public static class GameController
 
 		//create the players
 		switch (_aiSetting) {
+			case AIOption.Easy:
+				_ai = new AIEasyPlayer(_theGame);
+				break;
 			case AIOption.Medium:
 				_ai = new AIMediumPlayer(_theGame);
 				break;
@@ -83,7 +86,7 @@ public static class GameController
 				_ai = new AIHardPlayer(_theGame);
 				break;
 			default:
-				_ai = new AIHardPlayer(_theGame);
+				_ai = new AIEasyPlayer(_theGame);
 				break;
 		}
 
